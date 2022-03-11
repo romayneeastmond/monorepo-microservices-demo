330e7a08f11439c537e5051bcbdc955ee86d703f
Romayne Eastmond <romayne@gmail.com>
2022-03-11 08:02:07 -0700

    feat: Temporarily remove MassTransit and RabbitMQ.

--- Company.Employee/Company.Employee.Api/Program.cs
+++ Company.Employee/Company.Employee.Api/Program.cs
@@ -9,29 +9,8 @@ builder.Services.AddTransient<IEmployeeService, EmployeeService>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new() { Title = "Company.Employee.Api", Version = "v1" });
+c.SwaggerDoc("v1", new() { Title = "Company.Employee.Api", Version = "v1" });
 });
-
-builder.Services.AddMassTransit(config =>
-{
-    config.AddConsumer<EmployeeDepartmentDeletedService>();
-    config.AddConsumer<EmployeeNotificationListService>();
-
-    config.UsingRabbitMq((context, rabbitMqConfig) =>
-    {
-        rabbitMqConfig.ReceiveEndpoint("department-deleted", e =>
-        {
-             e.Consumer(() => new EmployeeDepartmentDeletedService());
-        });
-
-        rabbitMqConfig.ReceiveEndpoint("notification-list", e =>
-        {
-            e.Consumer(() => new EmployeeNotificationListService());
-        });
-    });
-});
-
-builder.Services.AddMassTransitHostedService();
 
 var app = builder.Build();
 
@@ -41,6 +20,6 @@ app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Company.Emp
 app.UseHttpsRedirection();
 
 app.AddApiRoutes();
-app.AddEventBusRoutes();
+app.AddEv
 
 app.Run();
