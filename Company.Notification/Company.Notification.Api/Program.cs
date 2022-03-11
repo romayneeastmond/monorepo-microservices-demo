e12b1f2227977b0df250928ef0aed678b9fcbcec
Romayne Eastmond <romayne@gmail.com>
2022-03-11 09:05:48 -0700

    feat: Readd MassTransit and RabbitMQ.

--- Company.Notification/Company.Notification.Api/Program.cs
+++ Company.Notification/Company.Notification.Api/Program.cs
@@ -9,8 +9,35 @@ builder.Services.AddTransient<INotificationService, NotificationService>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-c.SwaggerDoc("v1", new() { Title = "Company.Notification.Api", Version = "v1" });
+    c.SwaggerDoc("v1", new() { Title = "Company.Notification.Api", Version = "v1" });
 });
+
+builder.Services.AddMassTransit(config =>
+{
+    config.AddConsumer<NotificationCourseCreatedService>();
+    config.AddConsumer<NotificationEmployeeCreatedService>();
+    config.AddConsumer<NotificationEmployeeCourseBroadcastService>();
+
+    config.UsingRabbitMq((context, rabbitMqConfig) =>
+    {
+        rabbitMqConfig.ReceiveEndpoint("course-created", e =>
+        {
+            e.Consumer(() => new NotificationCourseCreatedService());
+        });
+
+        rabbitMqConfig.ReceiveEndpoint("employee-created", e =>
+        {
+            e.Consumer(() => new NotificationEmployeeCreatedService());
+        });
+
+        rabbitMqConfig.ReceiveEndpoint("employee-course-broadcast", e =>
+        {
+            e.Consumer(() => new NotificationEmployeeCourseBroadcastService());
+        });
+    });
+});
+
+builder.Services.AddMassTransitHostedService();
 
 var app = builder.Build();
 
