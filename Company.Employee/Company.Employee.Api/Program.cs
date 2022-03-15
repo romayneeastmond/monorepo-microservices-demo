6136f137780f00f38f1670b86a89bc04da57004d
Romayne Eastmond <romayne@gmail.com>
2022-03-14 20:50:08 -0600

    feat: Add environment variables for RabbitMQ.

--- Company.Employee/Company.Employee.Api/Program.cs
+++ Company.Employee/Company.Employee.Api/Program.cs
@@ -22,11 +22,33 @@ builder.Services.AddSwaggerGen(c =>
 
 builder.Services.AddMassTransit(config =>
 {
+    var rabbitMQServer = builder.Configuration.GetSection("RabbitMQConfiguration").GetSection("RabbitMQServer").Value;
+    var rabbitMQUsername = builder.Configuration.GetSection("RabbitMQConfiguration").GetSection("RabbitMQUsername").Value;
+    var rabbitMQPassword = builder.Configuration.GetSection("RabbitMQConfiguration").GetSection("RabbitMQPassword").Value;
+
+    if (string.IsNullOrWhiteSpace(rabbitMQServer))
+    {
+        rabbitMQServer = "localhost";
+    }
+
     config.AddConsumer<EmployeeDepartmentDeletedService>();
     config.AddConsumer<EmployeeNotificationListService>();
 
-    config.UsingRabbitMq((context, rabbitMqConfig) =>
+    config.AddBus(provider => Bus.Factory.CreateUsingRabbitMq(rabbitMqConfig =>
     {
+        rabbitMqConfig.Host(new Uri($"rabbitmq://{rabbitMQServer}:5672"), h =>
+        {
+            if (!string.IsNullOrWhiteSpace(rabbitMQUsername))
+            {
+                h.Username(rabbitMQUsername);
+            }
+
+            if (!string.IsNullOrWhiteSpace(rabbitMQPassword))
+            {
+                h.Password(rabbitMQPassword);
+            }
+        });
+
         rabbitMqConfig.ReceiveEndpoint("department-deleted", e =>
         {
             e.Consumer(() => new EmployeeDepartmentDeletedService());
@@ -36,7 +58,7 @@ builder.Services.AddMassTransit(config =>
         {
             e.Consumer(() => new EmployeeNotificationListService());
         });
-    });
+    }));
 });
 
 builder.Services.AddMassTransitHostedService();
--- Company.Notification/Company.Notification.Api/Program.cs
+++ Company.Notification/Company.Notification.Api/Program.cs
@@ -22,12 +22,34 @@ builder.Services.AddSwaggerGen(c =>
 
 builder.Services.AddMassTransit(config =>
 {
+    var rabbitMQServer = builder.Configuration.GetSection("RabbitMQConfiguration").GetSection("RabbitMQServer").Value;
+    var rabbitMQUsername = builder.Configuration.GetSection("RabbitMQConfiguration").GetSection("RabbitMQUsername").Value;
+    var rabbitMQPassword = builder.Configuration.GetSection("RabbitMQConfiguration").GetSection("RabbitMQPassword").Value;
+
+    if (string.IsNullOrWhiteSpace(rabbitMQServer))
+    {
+        rabbitMQServer = "localhost";
+    }
+
     config.AddConsumer<NotificationCourseCreatedService>();
     config.AddConsumer<NotificationEmployeeCreatedService>();
     config.AddConsumer<NotificationEmployeeCourseBroadcastService>();
 
-    config.UsingRabbitMq((context, rabbitMqConfig) =>
+    config.AddBus(provider => Bus.Factory.CreateUsingRabbitMq(rabbitMqConfig =>
     {
+        rabbitMqConfig.Host(new Uri($"rabbitmq://{rabbitMQServer}:5672"), h =>
+        {
+            if (!string.IsNullOrWhiteSpace(rabbitMQUsername))
+            {
+                h.Username(rabbitMQUsername);
+            }
+
+            if (!string.IsNullOrWhiteSpace(rabbitMQPassword))
+            {
+                h.Password(rabbitMQPassword);
+            }
+        });
+
         rabbitMqConfig.ReceiveEndpoint("course-created", e =>
         {
             e.Consumer(() => new NotificationCourseCreatedService());
@@ -42,7 +64,7 @@ builder.Services.AddMassTransit(config =>
         {
             e.Consumer(() => new NotificationEmployeeCourseBroadcastService());
         });
-    });
+    }));
 });
 
 builder.Services.AddMassTransitHostedService();
